a3e1f9b913efbdff48d4f3ca5b3cc4847e764724
Alpha1337k <ozziekruithof@gmail.com>
2021-11-17 16:49:33 +0100

    Feat: small leak cleanup, some still persist

--- src/parsing/Demofile.cpp
+++ src/parsing/Demofile.cpp
@@ -48,5 +48,71 @@ DemoFile::DemoFile(FILE *f)
 
 DemoFile::~DemoFile()
 {
+#define DeleteItem(type) \
+	case svc_##type: \
+	{ \
+		delete (type *)frames[i].pckt.msg[x].second; \
+		break; \
+	}
+#define DeleteNet(type) \
+	case net_##type: \
+	{ \
+		delete (type *)frames[i].pckt.msg[x].second; \
+		break; \
+	}
+std::cout << "Freeing" << std::endl;
+	for (size_t i = 0; i < frames.size(); i++)
+	{
+		for (size_t x = 0; x < frames[i].pckt.msg.size(); x++)
+		{
+			switch (frames[i].pckt.msg[x].first)
+			{
+				DeleteItem(GameEventList);
+				DeleteItem(GameEvent);
+				DeleteItem(ServerInfo);
+				DeleteItem(CreateStringTable);
+				DeleteItem(UpdateStringTable);
+				DeleteItem(UserMessage);
+				DeleteItem(PacketEntities);
 
+				DeleteNet(Disconnect);
+				DeleteNet(File);
+				DeleteNet(Tick);
+				DeleteNet(StringCmd);
+				DeleteNet(SetConVar);
+				DeleteNet(SignonState);
+
+				DeleteItem(ClassInfo);
+				DeleteItem(SetPause);
+				DeleteItem(VoiceInit);
+				DeleteItem(VoiceData);
+				DeleteItem(Print);
+				DeleteItem(Sounds);
+				DeleteItem(SetView);
+				DeleteItem(FixAngle);
+				DeleteItem(CrosshairAngle);
+				DeleteItem(BSPDecal);
+				DeleteItem(TempEntities);
+				DeleteItem(Prefetch);
+				DeleteItem(Menu);
+				DeleteItem(GetCvarValue);
+				case svc_DataTable:
+				{
+					DataTable *d = (DataTable *)frames[i].pckt.msg[x].second;
+					for (size_t y = 0; y < d->msg.size(); y++)
+					{
+						delete (SendTable *)d->msg[y].second;
+					}
+					
+				}
+			
+			default:
+				break;
+			}
+		}
+		
+	}
+#undef DeleteItem
+#undef DeleteNet
+	
 }
