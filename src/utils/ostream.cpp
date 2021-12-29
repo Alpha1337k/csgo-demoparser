5df4558427b43e5057b6ff1a76551b96280d0540
Alpha1337k <ozziekruithof@gmail.com>
2021-12-29 07:26:40 +0100

    Feat: removal of packet, unused

--- src/utils/ostream.cpp
+++ src/utils/ostream.cpp
@@ -55,59 +55,6 @@ std::ostream &operator<<(std::ostream &o, const DemHeader &d)
 	return (o);
 }
 
-std::ostream &operator<<(std::ostream &o, const Packet &p)
-{
-	o << "Packet {\n";
-	o << "flags:            " << p.splits[0].flags << std::endl;
-	// o << "viewOrigin:       " << p.splits[0].viewOrigin << std::endl;
-	// o << "viewAngles:       " << p.splits[0].viewAngles << std::endl;
-	// o << "localViewAngles:  " << p.splits[0].localViewAngles << std::endl;
-	// o << "viewOrigin:       " << p.splits[0].viewOrigin2 << std::endl;
-	// o << "viewAngles:       " << p.splits[0].viewAngles2 << std::endl;
-	// o << "localViewAngles:  " << p.splits[0].localViewAngles2 << std::endl;
-	// o << "flags:            " << p.splits[1].flags << std::endl;
-	// o << "viewOrigin:       " << p.splits[1].viewOrigin << std::endl;
-	// o << "viewAngles:       " << p.splits[1].viewAngles << std::endl;
-	// o << "localViewAngles:  " << p.splits[1].localViewAngles << std::endl;
-	// o << "viewOrigin:       " << p.splits[1].viewOrigin2 << std::endl;
-	// o << "viewAngles:       " << p.splits[1].viewAngles2 << std::endl;
-	// o << "localViewAngles:  " << p.splits[1].localViewAngles2 << std::endl;
-	o << "Messages: (" << p.msg.size() << ") [ \n";
-#define MessagePrinter(type) 						\
-	{												\
-	std::cout << "packet: " << #type << std::endl;	\
-	break;											\
-	}
-	for (size_t i = 0; i < p.msg.size(); i++)
-	{
-		switch (p.msg[i].first)
-		{
-			case svc_ServerInfo:
-				MessagePrinter(ServerInfo);
-			case svc_CreateStringTable:
-				MessagePrinter(CreateStringTable);
-			case svc_UpdateStringTable:
-				MessagePrinter(UpdateStringTable);
-			case svc_UserMessage:
-				MessagePrinter(UserMessage);
-			case svc_GameEvent:
-				MessagePrinter(GameEvent);
-			case svc_PacketEntities:
-				MessagePrinter(PacketEntities);
-			case svc_GameEventList:
-				MessagePrinter(GameEventList);
-			case svc_SendTable:
-				MessagePrinter(SendTable);
-			default:
-				MessagePrinter(NULL);
-		}
-	}
-#undef MessagePrinter
-	o << "] }";
-
-	return (o);
-}
-
 std::ostream &operator<<(std::ostream &o, const PropW &p)
 {
 	o << "Name: " << p.path << "\t";
@@ -160,7 +107,7 @@ std::ostream &operator<<(std::ostream &o, const Frame &f)
 	o << "cmd:        " << (int)f.cmd << std::endl;
 	o << "tick:       " << f.tick << std::endl;
 	o << "playerslot: " << (int)f.playerslot << std::endl;
-	o << "packet: {\n" << f.pckt << std::endl; 
+	// o << "packet: {\n" << f.pckt << std::endl; 
 	o << "} }\n";
 
 	return (o);
