4abd23363473bdcb1186d37e86ad46e84c0df987
Oscar Kruithof <ozziekruithof@gmail.com>
2022-04-07 12:14:55 +0200

    Feat: 10% faster stream, option for legacy variant

--- inc/StreamReader.hpp
+++ inc/StreamReader.hpp
@@ -3,118 +3,167 @@
 
 #include <string>
 
-const unsigned int s_nMaskTable[33] = {
-	0,
-	( 1 << 1 ) - 1,
-	( 1 << 2 ) - 1,
-	( 1 << 3 ) - 1,
-	( 1 << 4 ) - 1,
-	( 1 << 5 ) - 1,
-	( 1 << 6 ) - 1,
-	( 1 << 7 ) - 1,
-	( 1 << 8 ) - 1,
-	( 1 << 9 ) - 1,
-	( 1 << 10 ) - 1,
-	( 1 << 11 ) - 1,
-	( 1 << 12 ) - 1,
-	( 1 << 13 ) - 1,
-	( 1 << 14 ) - 1,
-	( 1 << 15 ) - 1,
-	( 1 << 16 ) - 1,
-	( 1 << 17 ) - 1,
-	( 1 << 18 ) - 1,
-	( 1 << 19 ) - 1,
-   	( 1 << 20 ) - 1,
-	( 1 << 21 ) - 1,
-	( 1 << 22 ) - 1,
-	( 1 << 23 ) - 1,
-	( 1 << 24 ) - 1,
-	( 1 << 25 ) - 1,
-	( 1 << 26 ) - 1,
-   	( 1 << 27 ) - 1,
-	( 1 << 28 ) - 1,
-	( 1 << 29 ) - 1,
-	( 1 << 30 ) - 1,
-	0x7fffffff,
-	0xffffffff,
+#ifdef USE_INTEGER
+#define size_type int
+#else
+#define size_type long
+#endif
+
+
+const unsigned size_type s_nMaskTable_l[65] = {
+    0,
+    ( (size_type)1 << 1 ) - 1,
+    ( (size_type)1 << 2 ) - 1,
+    ( (size_type)1 << 3 ) - 1,
+    ( (size_type)1 << 4 ) - 1,
+    ( (size_type)1 << 5 ) - 1,
+    ( (size_type)1 << 6 ) - 1,
+    ( (size_type)1 << 7 ) - 1,
+    ( (size_type)1 << 8 ) - 1,
+    ( (size_type)1 << 9 ) - 1,
+    ( (size_type)1 << 10 ) - 1,
+    ( (size_type)1 << 11 ) - 1,
+    ( (size_type)1 << 12 ) - 1,
+    ( (size_type)1 << 13 ) - 1,
+    ( (size_type)1 << 14 ) - 1,
+    ( (size_type)1 << 15 ) - 1,
+    ( (size_type)1 << 16 ) - 1,
+    ( (size_type)1 << 17 ) - 1,
+    ( (size_type)1 << 18 ) - 1,
+    ( (size_type)1 << 19 ) - 1,
+    ( (size_type)1 << 20 ) - 1,
+    ( (size_type)1 << 21 ) - 1,
+    ( (size_type)1 << 22 ) - 1,
+    ( (size_type)1 << 23 ) - 1,
+    ( (size_type)1 << 24 ) - 1,
+    ( (size_type)1 << 25 ) - 1,
+    ( (size_type)1 << 26 ) - 1,
+    ( (size_type)1 << 27 ) - 1,
+    ( (size_type)1 << 28 ) - 1,
+    ( (size_type)1 << 29 ) - 1,
+    ( (size_type)1 << 30 ) - 1,
+#ifdef USE_INTEGER
+	0x7FFFFFFF,
+	0xFFFFFFFF
+#else
+    ( (size_type)1 << 31 ) - 1,
+    ( (size_type)1 << 32 ) - 1,
+    ( (size_type)1 << 33 ) - 1,
+    ( (size_type)1 << 34 ) - 1,
+    ( (size_type)1 << 35 ) - 1,
+    ( (size_type)1 << 36 ) - 1,
+    ( (size_type)1 << 37 ) - 1,
+    ( (size_type)1 << 38 ) - 1,
+    ( (size_type)1 << 39 ) - 1,
+    ( (size_type)1 << 40 ) - 1,
+    ( (size_type)1 << 41 ) - 1,
+    ( (size_type)1 << 42 ) - 1,
+    ( (size_type)1 << 43 ) - 1,
+    ( (size_type)1 << 44 ) - 1,
+    ( (size_type)1 << 45 ) - 1,
+    ( (size_type)1 << 46 ) - 1,
+    ( (size_type)1 << 47 ) - 1,
+    ( (size_type)1 << 48 ) - 1,
+    ( (size_type)1 << 49 ) - 1,
+    ( (size_type)1 << 50 ) - 1,
+    ( (size_type)1 << 51 ) - 1,
+    ( (size_type)1 << 52 ) - 1,
+    ( (size_type)1 << 53 ) - 1,
+    ( (size_type)1 << 54 ) - 1,
+    ( (size_type)1 << 55 ) - 1,
+    ( (size_type)1 << 56 ) - 1,
+    ( (size_type)1 << 57 ) - 1,
+    ( (size_type)1 << 58 ) - 1,
+    ( (size_type)1 << 59 ) - 1,
+    ( (size_type)1 << 60 ) - 1,
+    ( (size_type)1 << 61 ) - 1,
+    ( (size_type)1 << 62 ) - 1,
+    0x7FFFFFFFFFFFFFFF,
+    0xFFFFFFFFFFFFFFFF
+#endif
 };
 
 class StreamReader
 {
 private:
-	const std::string &data;
-	int			idx;
-	char		bitsAvailable;
-	unsigned int buffer;
-	inline void		loadBuffer()
-	{
-		idx += sizeof(buffer);
-		buffer = *(int *)&data[idx];
-		bitsAvailable = 32;
-	}
+    const std::string &data;
+    int         idx;
+    char        bitsAvailable;
+    unsigned size_type buffer;
+    inline void     loadBuffer()
+    {
+        idx += sizeof(buffer);
+        buffer = *(size_type *)&data[idx];
+        bitsAvailable = sizeof(size_type) * 8;
+    }
 public:
-	~StreamReader() {}
+    ~StreamReader() {}
 
-	StreamReader(const std::string &d): 
-		data(d), idx(0), bitsAvailable(32), buffer(*(int *)&data[idx])
-	{
-		int diff = data.length() % 4;
+    StreamReader(const std::string &d): 
+        data(d), idx(0), bitsAvailable(64), buffer(*(size_type *)&data[idx])
+    {
+        int diff = data.length() % sizeof(size_type);
 
-		if (d.length() > 4 && diff != 0)
-		{
-			idx = diff - 4;
-			bitsAvailable = diff * 8;
-			buffer = (buffer << (32 - bitsAvailable)) >> (32 - bitsAvailable);
-		}
-		else if (d.length() <= 4)
-			bitsAvailable = d.length() * 8;
-		else
-			bitsAvailable = 32;
-	}
+        if (d.length() > sizeof(size_type) && diff != 0)
+        {
+            idx = diff - sizeof(size_type);
+            bitsAvailable = diff * 8;
+            buffer = (buffer << (sizeof(size_type) * 8 - bitsAvailable)) >> (sizeof(size_type) * 8 - bitsAvailable);
+        }
+        else if (d.length() <= 8)
+            bitsAvailable = d.length() * 8;
+        else
+            bitsAvailable = sizeof(size_type) * 8;
+    }
 
-	inline int			readBits(int len)
-	{
-		if (bitsAvailable >= len)
-		{
-			int rv = buffer & s_nMaskTable[len];
-			bitsAvailable -= len;
-			if (bitsAvailable) [[likely]]
-				buffer >>= len;
-			else
-				loadBuffer();
-			return rv;
-		}
-		else
-		{
-			unsigned int rv = buffer;
-			len -= bitsAvailable;
-			idx += sizeof(buffer);
-			buffer = *(int *)&data[idx];
-			rv |= ((buffer & s_nMaskTable[len]) << bitsAvailable);
-			bitsAvailable = 32 - len;
-			buffer >>= len;
+    inline int          readBits(int len)
+    {
+        if (bitsAvailable >= len)
+        {
+            int rv = buffer & s_nMaskTable_l[len];
+            bitsAvailable -= len;
+            if (bitsAvailable) [[likely]]
+                buffer >>= len;
+            else
+                loadBuffer();
+            return rv;
+        }
+        else
+        {
+            unsigned int rv = buffer;
+            len -= bitsAvailable;
+            idx += sizeof(buffer);
+            buffer = *(size_type *)&data[idx];
+            rv |= ((buffer & s_nMaskTable_l[len]) << bitsAvailable);
+            bitsAvailable = (sizeof(size_type) * 8) - len;
+            buffer >>= len;
 
-			return rv;
-		}
-	}
+            return rv;
+        }
+    }
 
-	inline int		readBit()
-	{
-		int rv = buffer & 1;
+    inline int      readBit()
+    {
+        int rv = buffer & 1;
 
-		bitsAvailable--;
-		if (bitsAvailable == 0) [[unlikely]]
-		{
-			loadBuffer();
-		}
-		else
-		{
-			buffer >>= 1;
-		}
-		return rv;
-	}
+        bitsAvailable--;
+        if (bitsAvailable == 0) [[unlikely]]
+        {
+            loadBuffer();
+        }
+        else
+        {
+            buffer >>= 1;
+        }
+        return rv;
+    }
 
-	inline int			readStreamInt()
+    inline bool     isEof()
+    {
+        return idx >= (int)data.length();
+    }
+
+		inline int			readStreamInt()
 	{
 		int ret = readBits(6);
 		switch (ret & (16 | 32)) {
@@ -129,11 +178,6 @@ public:
 				break;
 		}
 		return ret;
-	}
-
-	inline bool		isEof()
-	{
-		return idx >= (int)data.length();
 	}
 };
 
